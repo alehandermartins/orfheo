cddbb45101abbaf7f80badf945684a92ac1e9e92
Alehander <alejandro.marti@uv.es>
2016-02-07 12:01:57 +0100

    Register final version

--- assets/javascripts/widgets.js
+++ assets/javascripts/widgets.js
@@ -3,17 +3,17 @@
 
   ns.Widgets.Button = function(label, callback){
 
-    var _createdButton = $('<button>').attr({type:'button'}).text(label).click(callback);
+    var _createdWidget = $('<button>').attr({type:'button'}).text(label).click(callback);
 
     return {
       render: function(){
-        return _createdButton;
+        return _createdWidget;
       },
       disable: function(){
-        _createdButton.attr('disabled',true);
+        _createdWidget.attr('disabled',true);
       },
       enable: function(){
-        _createdButton.attr('disabled',false);
+        _createdWidget.attr('disabled',false);
       }
     };
   };
@@ -46,11 +46,54 @@
 
     var _createdWidget = $('<form>');
     var _invalidInput = $('<div>');
-    var regEx=/[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]/i;
+
     var _fields = {};
 
+    var regEx=/[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]/i;
     var _labels = ['Tu email', 'Confirma tu email', 'Contraseña'];
     var _types = ['email', 'email', 'password'];
+
+    ['email', 'emailConf', 'password'].forEach(function(id, index){
+      _fields[id] = Pard.Widgets.Input(_labels[index], _types[index], function(){
+
+        var _checkPassword = function(){
+          if(_fields['password'].getVal().length < 8){
+            _fields['password'].addWarning();
+            _invalidInput.text('La contraseña debe tener al menos 8 caracteres.');
+          }else{
+            _fields['password'].removeWarning();
+            return true;
+          }
+        }
+
+        var _checkEqual = function(){
+          if(_fields['email'].getVal() != _fields['emailConf'].getVal()){
+            _fields['emailConf'].addWarning();
+            _invalidInput.text('Los campos de correo no coinciden.');
+          }else{
+            _fields['emailConf'].removeWarning();
+            return _checkPassword();
+          }
+        }
+
+        var _checkInput = function(){
+          if(!regEx.test(_fields['email'].getVal())){
+            _fields['email'].addWarning();
+            _invalidInput.text('El correo debe tener un formato valido.');
+          }else{
+            _fields['email'].removeWarning();
+            _invalidInput.text('');
+            return _checkEqual();
+          }
+        }
+
+        if (_checkInput() == true){
+          _fields['button'].enable();
+        }else{
+          _fields['button'].disable();
+        }
+      });
+    });
 
     _fields['button'] = Pard.Widgets.Button('join the community', function(){
       Pard.Backend.register(
@@ -62,53 +105,11 @@
 
     _fields['button'].disable();
 
-    ['email', 'emailConf', 'password'].forEach(function(id, index){
-      _fields[id] = Pard.Widgets.Input(_labels[index], _types[index], function(){
-
-        var _checkPassword = function(){
-          if(_fields['password'].getVal().length < 8){
-            _fields['password'].addWarning();
-            _invalidInput.text('La contraseña debe tener al menos 8 caracteres.');
-          }else{
-            _fields['password'].removeWarning();
-          }
-        }
-
-        var _checkEqual = function(){
-          if(_fields['email'].getVal() != _fields['emailConf'].getVal()){
-            _fields['emailConf'].addWarning();
-            _invalidInput.text('Los campos de correo no coinciden.');
-          }else{
-          _fields['emailConf'].removeWarning();
-          _checkPassword();
-          }
-        }
-
-        var _checkInput = function(){
-          if(!regEx.test(_fields['email'].getVal())){
-            _fields['email'].addWarning();
-            _invalidInput.text('El correo debe tener un formato valido.');
-          }else{
-            _fields['email'].removeWarning();
-            _invalidInput.text('');
-            _checkEqual();
-          }
-        }
-
-      _checkInput();
-
-      if(_fields['email'].getVal().length != 0 && _invalidInput.text().length == 0)_fields['button'].enable();
-      if(_fields['email'].getVal().length == 0 || _invalidInput.text().length != 0) _fields['button'].disable();
-      });
+    Object.keys(_fields).map(function(field){
+      _createdWidget.append(_fields[field].render());
     });
 
-    _createdWidget.append(
-      _fields['email'].render(),
-      _fields['emailConf'].render(),
-      _fields['password'].render(),
-      _invalidInput,
-      _fields['button'].render()
-    );
+    _createdWidget.append(_invalidInput);
 
     return {
       render: function(){
