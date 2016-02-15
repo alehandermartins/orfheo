035e79d0afb6178f07c98343c4af7c82501cb3f9
Alehander <alejandro.marti@uv.es>
2016-02-15 14:47:39 +0100

    Calls repo accepts proposals as an element of an array

--- repos/calls.rb
+++ repos/calls.rb
@@ -18,13 +18,36 @@ module Repos
         results = @@calls_collection.find(query)
         return [] unless results.count > 0
 
-        results.map { |profile|
-          profile.map do |k,v|
-            next [k,v] unless k.is_a? String
-            next [k.to_sym, string_keyed_hash_to_symbolized(v)] if v.is_a? Hash
-            [k.to_sym, v]
-          end.to_h
+        results.map { |call|
+          string_keyed_hash_to_symbolized call
         }
+      end
+
+      def push query, proposal
+        @@calls_collection.update(query,{
+          "$push": {proposals: proposal}
+        })
+      end
+
+      private
+      def string_keyed_hash_to_symbolized hash
+        hash.map do |k,v|
+            next [k,v] unless k.is_a? String
+            next [k.to_sym, symbolize_array(v)] if v.is_a? Array
+            [k.to_sym, v]
+        end.to_h
+      end
+
+      def symbolize_array array
+        new_array = []
+        array.each{ |proposal|
+          new_proposal = {}
+          proposal.map{ |key, value|
+            new_proposal[key.to_sym] = value
+          }
+          new_array.push(new_proposal)
+        }
+        new_array
       end
     end
   end
