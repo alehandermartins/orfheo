2ce880cdb837d33c9c90ff3044b34854d8b38ead
Alehander <alejandro.marti@uv.es>
2016-04-01 14:28:04 +0200

    facebook key local

--- assets/javascripts/multimedia.js
+++ assets/javascripts/multimedia.js
@@ -9,10 +9,10 @@
 
   	window.fbAsyncInit = function() {
     //Heroku
-    FB.init({appId: '196330040742409', status: true, cookie: true, xfbml: true});
+    //FB.init({appId: '196330040742409', status: true, cookie: true, xfbml: true});
     
     //Local
-    //FB.init({appId: '282340465430456', status: true, cookie: true, xfbml: true});
+    FB.init({appId: '282340465430456', status: true, cookie: true, xfbml: true});
   }
   $.ajaxSetup({cache: true});
   $.getScript(document.location.protocol + '//connect.facebook.net/en_US/all.js');
