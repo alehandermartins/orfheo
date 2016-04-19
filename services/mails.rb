1c04ab28626c6d5df8a15e834ddac1d2cc2b2074
Alehander <alejandro.marti@uv.es>
2016-04-19 18:09:58 +0200

    accents in masil tryout

--- services/mails.rb
+++ services/mails.rb
@@ -24,14 +24,14 @@ module Services
         def welcome user
           Pony.options = {
             subject: 'Welcome to pard',
-            body: "<p> Bienvenido a Pard, para continuar con la inscripci\162n en el conFusi\162n activa tu cuenta con el siguiente enlace</p> <p><a href=\"http://pard.herokuapp.com/login/validate/#{user[:validation_code]}\">Activa tu cuenta</a> </p>"
+            body: "<p> Bienvenido a Pard, para continuar con la inscripci\xF3n en el conFusi\xF3n activa tu cuenta con el siguiente enlace</p> <p><a href=\"http://pard.herokuapp.com/login/validate/#{user[:validation_code]}\">Activa tu cuenta</a> </p>"
           }
         end
 
         def forgotten_password user
           Pony.options = {
             subject: 'Forgotten Password',
-            body: "<p> Puedes acceder a tu p\160gina de usuario a trav\130s del siguiente enlace </p> <p> <a href=\"http://pard.herokuapp.com/login/validate/#{user[:validation_code]}\">Accede a tu p\160gina</a></p> <p> Este enlace s\162lo es v\160lido una vez. Recuerda modificar tu contrase\164a una vez dentro. </p>"
+            body: "<p> Puedes acceder a tu p\xE1gina de usuario a trav\xE9s del siguiente enlace </p> <p> <a href=\"http://pard.herokuapp.com/login/validate/#{user[:validation_code]}\">Accede a tu p\xE1gina</a></p> <p> Este enlace s\xF3lo es v\xE1lido una vez. Recuerda modificar tu contrase\xF1a una vez dentro. </p>"
           }
         end
       end
