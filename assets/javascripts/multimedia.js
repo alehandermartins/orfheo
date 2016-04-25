3ec776b4296b02f791e7c74f96c3b20c4651c2e4
Alehander <alejandro.marti@uv.es>
2016-04-25 17:29:37 +0200

    documetn on load

--- assets/javascripts/multimedia.js
+++ assets/javascripts/multimedia.js
@@ -10,8 +10,8 @@
         var pinterestScript = $('<script>').attr({'src': '//assets.pinterest.com/js/pinit.js', 'type': 'text/javascript', 'data-pin-build': 'doBuild'});
         $('body').append(pinterestScript);
         $(document).ready(function(){
-          FB.init({appId: '196330040742409', status: true, cookie: true, xfbml: true});
-          //FB.init({appId: '282340465430456', status: true, cookie: true, xfbml: true});
+          //FB.init({appId: '196330040742409', status: true, cookie: true, xfbml: true});
+          FB.init({appId: '282340465430456', status: true, cookie: true, xfbml: true});
           callback();
         });
       }).fail(function(){
@@ -66,9 +66,11 @@
         });
         _imageContainer.prepend(_imageTitle);
       }
-      FB.XFBML.parse();
-      window.instgrm.Embeds.process();
-      doBuild();
+      $(document).ready(function(){
+        FB.XFBML.parse();
+        window.instgrm.Embeds.process();
+        doBuild();
+      });
     });
 
     return{
