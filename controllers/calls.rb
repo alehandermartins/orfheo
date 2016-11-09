61ea7012a626b15df3f04d64898c408cefd3f2f1
Alehander <alejandro.marti@uv.es>
2016-11-09 14:37:26 +0100

    form_category variable

--- controllers/calls.rb
+++ controllers/calls.rb
@@ -1,7 +1,7 @@
 class CallsController < BaseController
 
   post '/users/send_artist_proposal' do
-    scopify event_id: true, call_id: true, profile_id: true, production_id: true, category: true
+    scopify event_id: true, call_id: true, profile_id: true, production_id: true, category: true, form_category: true: true
     check_event_exists! event_id
     check_call_exists! call_id
     check_profile_ownership profile_id
@@ -14,21 +14,21 @@ class CallsController < BaseController
       Repos::Profiles.add_production profile_id, production.to_h
     end
 
-    form = get_artist_form call_id, category
+    form = get_artist_form call_id, form_category
     proposal = ArtistProposal.new(params, session[:identity], form)
     Repos::Events.add_artist event_id, proposal.to_h
     success ({profile_id: profile_id})
   end
 
   post '/users/send_space_proposal' do
-    scopify event_id: true, call_id: true, profile_id: true, category: true
+    scopify event_id: true, call_id: true, profile_id: true, category: true, form_category: true
     check_event_exists! event_id
     check_call_exists! call_id
     check_profile_ownership profile_id
     check_space_category! category
     check_deadline! event_id
 
-    form = get_space_form call_id, category
+    form = get_space_form call_id, form_category
     proposal = SpaceProposal.new(params, session[:identity], form)
     Repos::Events.add_space event_id, proposal.to_h
     success ({profile_id: profile_id})
@@ -53,26 +53,26 @@ class CallsController < BaseController
   end
 
   post '/users/modify_artist_proposal' do
-    scopify event_id: true, call_id: true, profile_id: true, proposal_id: true, category: true
+    scopify event_id: true, call_id: true, proposal_id: true, category: true, form_category: true
     check_event_ownership! event_id
     check_proposal_exists! proposal_id
     check_call_exists! call_id
     check_artist_category! category
 
-    form = get_artist_form call_id, category
+    form = get_artist_form call_id, form_category
     proposal = ArtistProposal.new(params, session[:identity], form)
     Repos::Events.modify_artist proposal.to_h
     success ({profile_id: profile_id})
   end
 
   post '/users/modify_space_proposal' do
-    scopify event_id: true, call_id: true, proposal_id: true, category: true
+    scopify event_id: true, call_id: true, proposal_id: true, category: true, form_category: true
     check_event_ownership! event_id
     check_proposal_exists! proposal_id
     check_call_exists! call_id
     check_space_category! category
 
-    form = get_space_form call_id, category
+    form = get_space_form call_id, form_category
     proposal = SpaceProposal.new(params, session[:identity], form)
     Repos::Events.modify_space proposal.to_h
     success ({profile_id: profile_id})
@@ -119,18 +119,18 @@ class CallsController < BaseController
     raise Pard::Invalid::UnexistingCall unless Repos::Calls.exists? call_id
   end
 
-  def get_artist_form call_id, category
+  def get_artist_form call_id, form_category
     forms = Repos::Calls.get_forms call_id
     categories = forms[:artist].keys
-    raise Pard::Invalid::Params unless categories.include? category.to_sym
-    forms[:artist][category.to_sym]
+    raise Pard::Invalid::Params unless categories.include? form_category.to_sym
+    forms[:artist][form_category.to_sym]
   end
 
-  def get_space_form call_id, category
+  def get_space_form call_id, form_category
     forms = Repos::Calls.get_forms call_id
     categories = forms[:space].keys
-    raise Pard::Invalid::Params unless categories.include? category.to_sym
-    forms[:space][category.to_sym]
+    raise Pard::Invalid::Params unless categories.include? form_category.to_sym
+    forms[:space][form_category.to_sym]
   end
 
   def check_proposal_exists! proposal_id
