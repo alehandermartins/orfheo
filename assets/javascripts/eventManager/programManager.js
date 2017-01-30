64402421953cf2021834fd99e869c5dd374cbfe1
alehandermartins <alehander.marti@gmail.com>
2017-01-30 22:51:19 +0100

    chain reactions almost fixed... only confirmed left

--- assets/javascripts/eventManager/programManager.js
+++ assets/javascripts/eventManager/programManager.js
@@ -895,15 +895,10 @@
           spaceSelector.append(spaceOption);
         });
 
-        daySelector = $('<select>');
-        daySelectorContainer.append(daySelector);
-        daySelector.select2({
-          dropdownCssClass: 'orfheoTableSelector'
-        }).on('select2:select', function(){
-          performance.date = daySelector.val();
-          var dateArray = daySelector.val().split('-');
-          var start = new Date(performance.time[0]);
-          var end = new Date(performance.time[1]);
+        var _changeDate = function(new_start, new_end){
+          var dateArray = performance.date.split('-');
+          var start = new Date(new_start);
+          var end = new Date(new_end);
 
           start.setUTCFullYear(parseInt(dateArray[0]));
           end.setUTCFullYear(parseInt(dateArray[0]));
@@ -916,11 +911,22 @@
 
           performance.time[0] = start.getTime();
           performance.time[1] = end.getTime();
+          console.log(moment(performance.time[0]).locale('es').format('DD-MM-YYYY'));
+          console.log(moment(performance.time[1]).locale('es').format('DD-MM-YYYY'));
+          setStartTimes();
+          setEndTimes();
+        }
+
+        daySelector = $('<select>');
+        daySelectorContainer.append(daySelector);
+        daySelector.select2({
+          dropdownCssClass: 'orfheoTableSelector'
+        }).on('select2:select', function(){
+          performance.date = daySelector.val();
+          _changeDate(performance.time[0], performance.time[1]);
           Pard.Backend.modifyPerformances(the_event.event_id, [performance], function(data){
             console.log('modify');
           });
-          setStartTimes();
-          setEndTimes();
         });
 
         daySelector.on('reload', function(e, date){
@@ -952,19 +958,16 @@
           }
         });
 
+        var _spaceSelect = function(host_id){
+          the_event.spaces[performance.host_id].deletePerformance(performance);
+          performance.host_id = host_id;
+        }
+
         spaceSelector.select2({
           dropdownCssClass: 'orfheoTableSelector'
         })
           .on('select2:select', function(){
-            the_event.spaces[performance.host_id].deletePerformance(performance);
-            var space = the_event.spaces[spaceSelector.val()].space;
-            performance.host_name = space.name;
-            performance.host_email = space.email;
-            performance.address = space.address;
-            performance.host_category = space.category;
-            performance.host_subcategory = space.subcategory;
-            performance.host_proposal_id = space.proposal_id;
-            performance.host_id = spaceSelector.val();
+            _spaceSelect(spaceSelector.val());
           });
 
         var setStartTimes = function(){
@@ -990,12 +993,22 @@
             .on('select2:select', function(){
               performance.time[0] = parseInt(startTime.select2('data')[0].time);
               if (performance.time[0] >= performance.time[1]) {
-                performance.time[1] = performance.time[0] + 15*60000;
+                performance.time[1] = performance.time[0] + 15 * 60000;
                 endTime.val(moment(parseInt(performance.time[1])).locale('es').format('HH:mm'))
                   .trigger('change');
               }
             });
-          startTime.val(moment(parseInt(performance.time[0])).locale('es').format('HH:mm')).trigger('change');
+
+          startTime.on('reload', function(){
+            performance.time[0] = parseInt(startTime.select2('data')[0].time);
+            startTime.val(moment(performance.time[0]).locale('es').format('HH:mm')).trigger('change');
+            if (performance.time[0] >= performance.time[1]) {
+              performance.time[1] = performance.time[0] + 15 * 60000;
+              endTime.val(moment(parseInt(performance.time[1])).locale('es').format('HH:mm'))
+                .trigger('change');
+            }
+          });
+          startTime.val(moment(performance.time[0]).locale('es').format('HH:mm')).trigger('change');
         }
 
         var setEndTimes = function(){
@@ -1025,6 +1038,16 @@
                   .trigger('change');
               }
             });
+
+          endTime.on('reload', function(){
+            performance.time[1] = parseInt(endTime.select2('data')[0].time);
+            endTime.val(moment(performance.time[1]).locale('es').format('HH:mm')).trigger('change');
+            if (performance.time[1] <= performance.time[0]) {
+              performance.time[0] = performance.time[1] - 15 * 60000;
+              startTime.val(moment(parseInt(performance.time[0])).locale('es').format('HH:mm'))
+                .trigger('change');
+            }
+          });
           endTime.val(moment(parseInt(performance.time[1])).locale('es').format('HH:mm')).trigger('change');
         }
 
@@ -1068,6 +1091,7 @@
           },
           daySelector: daySelector,
           spaceSelector: spaceSelector,
+          spaceSelect: _spaceSelect,
           input: input,
           startTime: startTime,
           endTime: endTime, 
@@ -1138,9 +1162,8 @@
                 if (_manager != manager){
                   manager.spaceSelector.val(val)
                     .trigger('change');
+                  manager.spaceSelect(val);
                   var _show = the_event.program[id].show;
-                  the_event.spaces[_show.host_id].deletePerformance(_show);
-                  _show.host_id = val;
                   shows.push(_show);
                 }
               }
@@ -1158,18 +1181,15 @@
                 var manager = _managers.collection[id].manager;
                 if (_manager != manager){
                   manager.startTime.val(val)
-                  .trigger('change');
+                    .trigger('reload');
+                  var _show = the_event.program[id].show;
+                  shows.push(_show);              
                 }
               }
-              Pard.Backend.modifyPerformances(the_event.event_id, artistShows(), function(data){
-                console.log('modify');
-              });
             }
-            else{
-              Pard.Backend.modifyPerformances(the_event.event_id, [show], function(data){
-                console.log('modify');
-              }); 
-            }      
+            Pard.Backend.modifyPerformances(the_event.event_id, shows, function(data){
+              console.log('modify');
+            }); 
           });
 
           _manager.endTime.on('select2:select',function(e, state){
@@ -1180,8 +1200,9 @@
                 var manager = _managers.collection[id].manager;
                 if (_manager != manager){
                   manager.endTime.val(val)
-                  .trigger('change');
+                    .trigger('reload');
                   var _show = the_event.program[id].show;
+                  shows.push(_show);
                 }
               }
             }
@@ -1191,14 +1212,17 @@
           });
 
           _manager.input.click(function(){
+            var shows = [show];
             for (var id in _managers.collection){
               var manager = _managers.collection[id].manager;
               if (manager != _manager){
                 manager.input.prop("checked", _manager.input.is(":checked"));
                 manager.input.trigger('change');
+                var _show = the_event.program[id].show;
+                shows.push(_show);
               }
             }
-            Pard.Backend.modifyPerformances(the_event.event_id, artistShows(), function(data){
+            Pard.Backend.modifyPerformances(the_event.event_id, shows, function(data){
               console.log('modify');
             });
           });
