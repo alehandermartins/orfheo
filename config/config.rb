be5161e776f6742a950da087a9f0352e5dead885
alehandermartins <alehander.marti@gmail.com>
2017-01-31 20:46:38 +0100

    removing test sending mails

--- config/config.rb
+++ config/config.rb
@@ -85,6 +85,20 @@ class BaseController < Sinatra::Base
     }
   end
 
+  options = {
+    :headers => { 'Content-Type' => 'text/html' },
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => zz'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  }
+
   # options = {
   #   :headers => { 'Content-Type' => 'text/html' },
   #   :via => :smtp,
@@ -92,33 +106,18 @@ class BaseController < Sinatra::Base
   #     :address => 'smtp.sendgrid.net',
   #     :port => '587',
   #     :domain => 'heroku.com',
-  #     :user_name => ENV['SENDGRID_USERNAME'],
-  #     :password => ENV['SENDGRID_PASSWORD'],
+  #     :user_name => 'app47085092@heroku.com',
+  #     :password => 'a9awf3mj5410',
   #     :authentication => :plain,
   #     :enable_starttls_auto => true
   #   }
   # }
 
-  options = {
-    :headers => { 'Content-Type' => 'text/html' },
-    :via => :smtp,
-    :via_options => {
-      :address => 'smtp.sendgrid.net',
-      :port => '587',
-      :domain => 'heroku.com',
-      :user_name => 'app47085092@heroku.com',
-      :password => 'a9awf3mj5410',
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-  }
-
   Mongo::Logger.logger.level = ::Logger::FATAL
 
   configure :development, :test do
     @@db = Mongo::Client.new('mongodb://localhost:27017/Orfheo/cg_dev')
-    #Pony.override_options = {:via => :test}
-    Pony.override_options = options
+    Pony.override_options = {:via => :test}
     Cloudinary.config do |config|
       config.cloud_name = 'hxgvncv7u'
       config.api_key = '844974134959653'
